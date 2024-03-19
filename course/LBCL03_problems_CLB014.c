db873b206a38190406ec23a5fcaaf2ad23f91187
Tanay-2024 <tanayroy7811@gmail.com>
2024-03-19 11:47:40 +0530

    my tenth code

---
+++ course/LBCL03_problems_CLB014.c
@@ -0,0 +1,14 @@
+// Task
+// Write a program which prints "7 and 3", using multiple %d in a single printf statement.
+
+// Expected Output
+
+// 7 and 3
+
+#include <stdio.h>
+
+int main() {
+	
+	printf("%d and %d",7,3);
+	return 0;
+}
