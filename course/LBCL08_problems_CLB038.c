fe16164f31f85cdf25084eea6e0ed428fe0f182e
Tanay-2024 <tanayroy7811@gmail.com>
2024-03-19 12:46:13 +0530

    code number eighteen

---
+++ course/LBCL08_problems_CLB038.c
@@ -0,0 +1,16 @@
+// Write a program which does the following
+
+// Create a string named fruit and assign the value "Apple" to it.
+// Output fruit to the console.
+
+
+#include <stdio.h>
+
+int main() {
+
+    char greet[] = "Apple";
+    printf("%s", greet);
+    return 0;
+    
+
+}
