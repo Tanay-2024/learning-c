1060c720053d6cc07b74a466c7becd65f749907c
Tanay-2024 <tanayroy7811@gmail.com>
2024-03-20 10:55:37 +0530

    code number twenty two

---
+++ course/LBCL11_problems_CLB054.c
@@ -0,0 +1,21 @@
+// Write a program which does the following
+
+// Declare an integer variable a
+// Try taking a number from the console and assign it to a
+// Output a to the console
+// Sample 1:
+// Input
+// Output
+// 76
+// Your number is:76
+
+#include <stdio.h>
+
+int main() {
+
+    int a;
+    scanf("%d", &a);
+    printf("Your number is: %d",76);
+    return 0;
+  
+}
